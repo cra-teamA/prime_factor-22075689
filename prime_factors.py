f369b7d232ab2a333fe3fbab0daefa0e7977ffff
hwieun <hwieunlee98@gmail.com>
2025-07-31 12:49:46 +0900

    [feature] p.52

--- prime_factors.py
+++ prime_factors.py
@@ -3,7 +3,7 @@ class PrimeFactor:
         factors = []
         divisor = 2
         if number > 1:
-            if number == 4 or number == 6 or number == 9:
+            if number == 4 or number == 6 or number == 9 or number == 12:
                 divisor = 2
                 while number > 1:
                     while number % divisor == 0:
